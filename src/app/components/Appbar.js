2f7037840f0374f5e37e95b4a592ea367e0a8d92
SergioHs <shs_28@hotmail.com>
2023-12-14 21:43:29 -0300

    Updates Appbar

--- src/app/components/Appbar.js
+++ src/app/components/Appbar.js
@@ -1,10 +1,20 @@
 import React,{ useContext, useEffect} from "react";
 import { ThemeContext } from "@/app/contexts/ThemeContext"
-import { AuthContext } from '@/app/contexts/AuthContext';
+import { AuthContext, AuthProvider, useAuth } from '@/app/contexts/AuthContext';
+import { useRouter } from "next/router";
 
 function Appbar({onMenuToggle}) {
   const { theme } = useContext(ThemeContext);
   const { userInfo } = useContext(AuthContext);
+  const router = useRouter();
+
+  const authContext = useAuth();
+  const { logout } = authContext;
+
+  const onLogout = async () => {
+      logout(null)
+      router.push(`/login`);
+  };
 
   useEffect(() => {
   }, [theme]);
@@ -22,8 +32,13 @@ function Appbar({onMenuToggle}) {
     <div className={`flex justify-between items-center p-4`}>
       <div className={`mx-4
       ${theme === 'dark' ? 'text-white' : 'text-black'}`}>
-        <h1>Logado(a) como: {userInfo && userInfo.user.user_name}</h1>
+        <h1> {userInfo && 'Logado(a) como:' + userInfo.user.user_name}</h1>
       </div>
+
+      <button className="mx-4" onClick={onLogout}>
+        {userInfo && 'Logout'}
+      </button>
+
     <button onClick={onMenuToggle}>
       <svg className="w-6 h-6 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
